09af00bfd62173fdb3bd0dfb47bb130cef15878d
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2025-08-07 19:57:53 +0300

    Shadcn UI library init

---
+++ system/app/(auth)/auth/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "../(components)/LoginForm"
+
+export default function AuthPage () { 
+    return ( 
+        <div>
+            Auth Page
+            <LoginForm /> 
+        </div>
+    )
+}
